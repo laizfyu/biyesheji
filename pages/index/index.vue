a98cfd6ac23627a0eeb57e6a36d8d51c65cf5df0
laizfyu <2362187949@qq.com>
2022-03-15 16:23:48 +0800

    合并

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -15,8 +15,8 @@
       </view>
     </view>
     <view class="shouna_center">
-      <view class="echarts-wrap">
-          <mpvue-echarts :echarts="echarts" :onInit="onInit" canvasId="demo-canvas" />
+      <view class="charts-box">
+      	<qiun-data-charts type="column" :localdata="localdata" />
       </view>
      <!-- <view class="shouna_item" v-for="(item,index) in list" :key="item.index" @click="getWupin(index)">
         <view class="item_head">
@@ -75,66 +75,6 @@
     },
     methods: {
       
-      // initChart(canvas, width, height) {
-      //   let chart = null;
-      //   chart = echarts.init(canvas, null, {
-      //     width: width,
-      //     height: height
-      //   });
-      //   canvas.setChart(chart);
-       
-      //   var option = {
-      //     title: {
-      //       text: 'Referer of a Website',
-      //       subtext: 'Fake Data',
-      //       left: 'center'
-      //     },
-      //     tooltip: {
-      //       trigger: 'item'
-      //     },
-      //     legend: {
-      //       orient: 'vertical',
-      //       left: 'left'
-      //     },
-      //     series: [{
-      //       name: 'Access From',
-      //       type: 'pie',
-      //       radius: '50%',
-      //       data: [{
-      //           value: 1048,
-      //           name: 'Search Engine'
-      //         },
-      //         {
-      //           value: 735,
-      //           name: 'Direct'
-      //         },
-      //         {
-      //           value: 580,
-      //           name: 'Email'
-      //         },
-      //         {
-      //           value: 484,
-      //           name: 'Union Ads'
-      //         },
-      //         {
-      //           value: 300,
-      //           name: 'Video Ads'
-      //         }
-      //       ],
-      //       emphasis: {
-      //         itemStyle: {
-      //           shadowBlur: 10,
-      //           shadowOffsetX: 0,
-      //           shadowColor: 'rgba(0, 0, 0, 0.5)'
-      //         }
-      //       }
-      //     }]
-      //   }; // ECharts 配置项
-       
-      //   chart.setOption(option);
-       
-      //   return chart; // 返回 chart 后可以自动绑定触摸操作
-      // },
       // getList() {
       //   const users = uni.getStorageSync("userInfo");
       //   // console.log("用户信息:")
